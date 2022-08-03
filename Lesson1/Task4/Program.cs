a1c1a92f6692d73d5082ee757284b8b9c579aa0c
Cainit <dolgov2405@gmail.com>
2022-08-03 17:29:42 +1000

    Выполнил домашнюю работу

---
+++ Lesson1/Task4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*4. Написать программу обмена значениями двух переменных типа int без использования вспомогательных методов.
+            а) с использованием третьей переменной;
+            б) *без использования третьей переменной.
+
+            Студент - Константин Долгов */
+
+            Console.Title = "Обмен переменных";
+            int varOne = 10;
+            int varTwo = 20;
+
+            Console.WriteLine("Значения переменных до обмена: первая=" + varOne + ", вторая =" + varTwo );
+
+            //вариант а)
+            int varThree = varOne;
+            varOne = varTwo;
+            varTwo = varThree;
+            Console.WriteLine("Значения переменных для обмена а): первая=" + varOne + ", вторая =" + varTwo);
+
+            //вариант б)
+            varOne = varOne + varTwo;
+            varTwo = varTwo - varOne;
+            varTwo = -varTwo;
+            varOne = varOne - varTwo;
+            Console.WriteLine("Значения переменных для обмена б): первая=" + varOne + ", вторая =" + varTwo);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lesson1/Task5/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*5. а) Написать программу, которая выводит на экран ваше имя, фамилию и город проживания.
+            б) Сделать задание, только вывод организовать в центре экрана.
+            в) *Сделать задание б с использованием собственных методов (например, Print(string ms, int x,int y).
+
+            Студент - Константин Долгов*/
+
+            Console.Title = "Печать в центре экрана";
+
+            Console.Write("Введите ваше имя: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Введите вашу фамилию: ");
+            string surname = Console.ReadLine();
+
+            Console.Write("Введите город вашего проживания: ");
+            string city = Console.ReadLine();
+
+
+            string displayString = $"Вы {name} {surname}, и вы проживаете в городе {city}";
+
+            // варианта а)
+            Console.Write("Вариант а): "+displayString);
+
+            // вариант б) и в)
+            displayString = $"Вариант б,в): Вы {name} {surname}, и вы проживаете в городе {city}";
+            PrintString(displayString, Console.WindowWidth / 2 - displayString.Length / 2, Console.WindowHeight / 2);
+
+            Console.ReadLine();
+        }
+
+        static void PrintString(string str, int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(str);
+        }
+    }
+}
