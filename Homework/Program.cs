50fd5feb021803c05d74cfdba719f2dd4e942349
Cainit <dolgov2405@gmail.com>
2022-08-03 13:32:53 +1000

    Выполнил задачу 1

---
+++ Homework/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Анкета";   
+
+            Console.Write("Введите ваше имя: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Введите вашу фамилию: ");
+            string surname = Console.ReadLine();
+
+            Console.Write("Сколько вам лет? ");
+            double age = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Сколько сантиметров ваш рост? ");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Сколько киллограмов вы весите? ");
+            double weight = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Итак, Вы - "+ surname+" "+name+", вам "+age+" лет, ваш рост "+height+" сантиметров, а вес - "+weight+" килограмм");
+            Console.ReadLine();
+        }
+    }
+}
